8d62073cc0b891418463c3d594bdaef11630efa2
lia-cat <81067357+lia-cat@users.noreply.github.com>
2025-05-28 13:15:39 +0300

    create file

---
+++ create file.bat
@@ -0,0 +1,11 @@
+echo off
+rem go to into firstcat
+cd dmitrochenkova
+rem create firsfile
+echo > 07062004.txt
+rem go to into secondcat
+cd julia
+cd sergeevna
+rem create thirdfile
+echo > mynumber.txt
+pause
